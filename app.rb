955ac80ed85dc0e70b5659f427b2c56fb9756a96
Shuvangee Seth <144741662+shuvangee@users.noreply.github.com>
2024-08-01 22:58:36 +0000

    final commit

--- app.rb
+++ app.rb
@@ -2,8 +2,33 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  
+  erb(:homepage)
+  
 end
+
+
+
+get("/") do
+  erb(:homepage)
+  
+end
+
+get("/rock") do
+  @samp= ["rock", "paper", "scissors"].sample 
+  @outcome = "They played #{@samp}!"
+	erb(:rock)
+end 
+
+
+get("/paper") do
+  @samp= ["rock", "paper", "scissors"].sample 
+  @outcome = "They played #{@samp}!"
+	erb(:paper)
+end 
+
+get("/scissors") do
+  @samp= ["rock", "paper", "scissors"].sample 
+  @outcome = "They played #{@samp}!"
+	erb(:scissors)
+end 
